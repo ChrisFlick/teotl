eb8b3af29a24d2dc9918ad8d135d54ac4b5a4a0f
Chris Flick <chrisflick@gmail.com>
2019-06-21 13:29:15 -0700

    improved elementClick() so that player can select an element

---
+++ scripts/elementClick.js
@@ -0,0 +1,21 @@
+function elementClick(button) {
+    // Declare variables.
+    let image;
+    let source = "sprites/buttons/";
+    let extension = ".png"
+
+    // Revert all other buttons to their non clicked image.
+    for (let i = 0; i < 5; i++) {
+        let elementID = "element_" + String(i);
+        if (elementID != button) {
+            image = document.getElementById(elementID);
+            image.src = source + elementID + extension;
+        }
+    }
+
+    // Change image of button the player clicked to it's clicked variant.
+    image = document.getElementById(button);
+    image.src = source + button + "Clicked" + extension;
+
+    document.getElementById("log").innerHTML = button;
+}
