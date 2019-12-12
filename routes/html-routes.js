171d71aa0dc72ee13ea6b47e96784f30a7514aa3
Chris Flick <chrisflick@gmail.com>
2019-12-12 12:07:41 -0800

    Now serves menu.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,4 +7,24 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/levels/logo.html"))
     })
+
+    app.get("/arena", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/levels/arena.html"))
+    })
+
+    app.get("/character_select", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/levels/character_select.html"))
+    })
+
+    app.get("/menu", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/levels/menu.html"))
+    })
+
+    app.get("/pentacle", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/levels/pentacle.html"))
+    })
+
+    app.get("/wait", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/levels/waiting.html"))
+    })
 }
